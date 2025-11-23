72e0b3efa47ffcff5b1a4412aca158661feef414
Danny Pineault <danny.pineault@precisiondp.ca>
2025-11-23 13:26:42 -0500

    Fix: Imports corrects - default vs named exports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
-import { AuthProvider } from '@/hooks/useAuth'
-import { ProjectsProvider } from '@/hooks/useProjects'
+import { useAuth } from '@/hooks/useAuth'
+import { useProjects } from '@/hooks/useProjects'
 import { ToastProvider } from '@/components/ToastProvider'
 import { Login } from '@/pages/Login'
 import { Dashboard } from '@/pages/Dashboard'
@@ -8,9 +8,9 @@ import { ProjectDetails } from '@/pages/ProjectDetails'
 import { Takeoff } from '@/pages/Takeoff'
 import { Settings } from '@/pages/Settings'
 import { Pricing } from '@/pages/Pricing'
-import { CloudStorage } from '@/pages/CloudStorage'
-import { BidProposal } from '@/pages/BidProposal'
-import { ProjectCosts } from '@/pages/ProjectCosts'
+import CloudStorage from '@/pages/CloudStorage'
+import BidProposal from '@/pages/BidProposal'
+import ProjectCosts from '@/pages/ProjectCosts'
 
 // Pages Projets
 import { ProjetsConcept } from '@/pages/Projets/Conception'
@@ -38,8 +38,7 @@ import { ApplicationMobileTerrain } from '@/pages/OutilsAvances/ApplicationMobil
 import { MessagerieEquipe } from '@/pages/OutilsAvances/Messagerie'
 import { Geolocalisation } from '@/pages/OutilsAvances/Geolocalisation'
 
-import { AppHeader } from '@/components/AppHeader'
-import { useAuth } from '@/hooks/useAuth'
+import AppHeader from '@/components/AppHeader'
 
 function PrivateRoute({ children }: { children: React.ReactNode }) {
   const { user, loading } = useAuth()
@@ -69,212 +68,208 @@ function PrivateLayout({ children }: { children: React.ReactNode }) {
 export default function App() {
   return (
     <Router>
-      <AuthProvider>
-        <ProjectsProvider>
-          <ToastProvider>
-            <Routes>
-              {/* Public Routes */}
-              <Route path="/login" element={<Login />} />
-              <Route path="/pricing" element={<Pricing />} />
+      <ToastProvider>
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/pricing" element={<Pricing />} />
 
-              {/* Private Routes - Dashboard */}
-              <Route path="/dashboard" element={
-                <PrivateRoute>
-                  <PrivateLayout>
-                    <Dashboard />
-                  </PrivateLayout>
-                </PrivateRoute>
-              } />
+          {/* Private Routes - Dashboard */}
+          <Route path="/dashboard" element={
+            <PrivateRoute>
+              <PrivateLayout>
+                <Dashboard />
+              </PrivateLayout>
+            </PrivateRoute>
+          } />
 
-              {/* Private Routes - Projects */}
-              <Route path="/project/:projectId" element={
-                <PrivateRoute>
-                  <PrivateLayout>
-                    <ProjectDetails />
-                  </PrivateLayout>
-                </PrivateRoute>
-              } />
+          {/* Private Routes - Projects */}
+          <Route path="/project/:projectId" element={
+            <PrivateRoute>
+              <PrivateLayout>
+                <ProjectDetails />
+              </PrivateLayout>
+            </PrivateRoute>
+          } />
 
-              <Route path="/takeoff/:projectId" element={
-                <PrivateRoute>
-                  <PrivateLayout>
-                    <Takeoff />
-                  </PrivateLayout>
-                </PrivateRoute>
-              } />
+          <Route path="/takeoff/:projectId" element={
+            <PrivateRoute>
+              <PrivateLayout>
+                <Takeoff />
+              </PrivateLayout>
+            </PrivateRoute>
+          } />
 
-              <Route path="/cloud-storage/:projectId" element={
-                <PrivateRoute>
-                  <PrivateLayout>
-                    <CloudStorage />
-                  </PrivateLayout>
-                </PrivateRoute>
-              } />
+          <Route path="/cloud-storage/:projectId" element={
+            <PrivateRoute>
+              <PrivateLayout>
+                <CloudStorage />
+              </PrivateLayout>
+            </PrivateRoute>
+          } />
 
-              <Route path="/bid-proposal/:projectId" element={
-                <PrivateRoute>
-                  <PrivateLayout>
-                    <BidProposal />
-                  </PrivateLayout>
-                </PrivateRoute>
-              } />
+          <Route path="/bid-proposal/:projectId" element={
+            <PrivateRoute>
+              <PrivateLayout>
+                <BidProposal />
+              </PrivateLayout>
+            </PrivateRoute>
+          } />
 
-              <Route path="/project-costs/:projectId" element={
-                <PrivateRoute>
-                  <PrivateLayout>
-                    <ProjectCosts />
-                  </PrivateLayout>
-                </PrivateRoute>
-              } />
+          <Route path="/project-costs/:projectId" element={
+            <PrivateRoute>
+              <PrivateLayout>
+                <ProjectCosts />
+              </PrivateLayout>
+            </PrivateRoute>
+          } />
 
-              {/* Private Routes - Projets Menu */}
-              <Route path="/projets/conception" element={
-                <PrivateRoute>
-                  <PrivateLayout>
-                    <ProjetsConcept />
-                  </PrivateLayout>
-                </PrivateRoute>
-              } />
+          {/* Private Routes - Projets Menu */}
+          <Route path="/projets/conception" element={
+            <PrivateRoute>
+              <PrivateLayout>
+                <ProjetsConcept />
+              </PrivateLayout>
+            </PrivateRoute>
+          } />
 
-              <Route path="/projets/estimation" element={
-                <PrivateRoute>
-                  <PrivateLayout>
-                    <ProjetsEstimation />
-                  </PrivateLayout>
-                </PrivateRoute>
-              } />
+          <Route path="/projets/estimation" element={
+            <PrivateRoute>
+              <PrivateLayout>
+                <ProjetsEstimation />
+              </PrivateLayout>
+            </PrivateRoute>
+          } />
 
-              <Route path="/projets/gestion" element={
-                <PrivateRoute>
-                  <PrivateLayout>
-                    <ProjetsGestion />
-                  </PrivateLayout>
-                </PrivateRoute>
-              } />
+          <Route path="/projets/gestion" element={
+            <PrivateRoute>
+              <PrivateLayout>
+                <ProjetsGestion />
+              </PrivateLayout>
+            </PrivateRoute>
+          } />
 
-              {/* Private Routes - Entrepreneurs */}
-              <Route path="/entrepreneurs/rbq" element={
-                <PrivateRoute>
-                  <PrivateLayout>
-                    <EntrepreneursRBQ />
-                  </PrivateLayout>
-                </PrivateRoute>
-              } />
+          {/* Private Routes - Entrepreneurs */}
+          <Route path="/entrepreneurs/rbq" element={
+            <PrivateRoute>
+              <PrivateLayout>
+                <EntrepreneursRBQ />
+              </PrivateLayout>
+            </PrivateRoute>
+          } />
 
-              <Route path="/entrepreneurs/personnel" element={
-                <PrivateRoute>
-                  <PrivateLayout>
-                    <EntrepreneursPersonnel />
-                  </PrivateLayout>
-                </PrivateRoute>
-              } />
+          <Route path="/entrepreneurs/personnel" element={
+            <PrivateRoute>
+              <PrivateLayout>
+                <EntrepreneursPersonnel />
+              </PrivateLayout>
+            </PrivateRoute>
+          } />
 
-              {/* Private Routes - Appels d'offre */}
-              <Route path="/appels-offre/seao" element={
-                <PrivateRoute>
-                  <PrivateLayout>
-                    <AppelsOffreSEAO />
-                  </PrivateLayout>
-                </PrivateRoute>
-              } />
+          {/* Private Routes - Appels d'offre */}
+          <Route path="/appels-offre/seao" element={
+            <PrivateRoute>
+              <PrivateLayout>
+                <AppelsOffreSEAO />
+              </PrivateLayout>
+            </PrivateRoute>
+          } />
 
-              <Route path="/appels-offre/merx" element={
-                <PrivateRoute>
-                  <PrivateLayout>
-                    <AppelsOffreMERX />
-                  </PrivateLayout>
-                </PrivateRoute>
-              } />
+          <Route path="/appels-offre/merx" element={
+            <PrivateRoute>
+              <PrivateLayout>
+                <AppelsOffreMERX />
+              </PrivateLayout>
+            </PrivateRoute>
+          } />
 
-              <Route path="/appels-offre/buy-gc" element={
-                <PrivateRoute>
-                  <PrivateLayout>
-                    <AppelsOffreBuyGC />
-                  </PrivateLayout>
-                </PrivateRoute>
-              } />
+          <Route path="/appels-offre/buy-gc" element={
+            <PrivateRoute>
+              <PrivateLayout>
+                <AppelsOffreBuyGC />
+              </PrivateLayout>
+            </PrivateRoute>
+          } />
 
-              <Route path="/appels-offre/bonfire" element={
-                <PrivateRoute>
-                  <PrivateLayout>
-                    <AppelsOffreBonfire />
-                  </PrivateLayout>
-                </PrivateRoute>
-              } />
+          <Route path="/appels-offre/bonfire" element={
+            <PrivateRoute>
+              <PrivateLayout>
+                <AppelsOffreBonfire />
+              </PrivateLayout>
+            </PrivateRoute>
+          } />
 
-              {/* Private Routes - Ressources */}
-              <Route path="/ressources/code-navigator" element={
-                <PrivateRoute>
-                  <PrivateLayout>
-                    <CodeNavigator />
-                  </PrivateLayout>
-                </PrivateRoute>
-              } />
+          {/* Private Routes - Ressources */}
+          <Route path="/ressources/code-navigator" element={
+            <PrivateRoute>
+              <PrivateLayout>
+                <CodeNavigator />
+              </PrivateLayout>
+            </PrivateRoute>
+          } />
 
-              <Route path="/ressources/ccq-navigator" element={
-                <PrivateRoute>
-                  <PrivateLayout>
-                    <CCQNavigator />
-                  </PrivateLayout>
-                </PrivateRoute>
-              } />
+          <Route path="/ressources/ccq-navigator" element={
+            <PrivateRoute>
+              <PrivateLayout>
+                <CCQNavigator />
+              </PrivateLayout>
+            </PrivateRoute>
+          } />
 
-              <Route path="/ressources/documents-acc-ccdc" element={
-                <PrivateRoute>
-                  <PrivateLayout>
-                    <DocumentsACCCDC />
-                  </PrivateLayout>
-                </PrivateRoute>
-              } />
+          <Route path="/ressources/documents-acc-ccdc" element={
+            <PrivateRoute>
+              <PrivateLayout>
+                <DocumentsACCCDC />
+              </PrivateLayout>
+            </PrivateRoute>
+          } />
 
-              <Route path="/ressources/associations" element={
-                <PrivateRoute>
-                  <PrivateLayout>
-                    <Associations />
-                  </PrivateLayout>
-                </PrivateRoute>
-              } />
+          <Route path="/ressources/associations" element={
+            <PrivateRoute>
+              <PrivateLayout>
+                <Associations />
+              </PrivateLayout>
+            </PrivateRoute>
+          } />
 
-              {/* Private Routes - Outils Avancés */}
-              <Route path="/outils-avances/application-mobile" element={
-                <PrivateRoute>
-                  <PrivateLayout>
-                    <ApplicationMobileTerrain />
-                  </PrivateLayout>
-                </PrivateRoute>
-              } />
+          {/* Private Routes - Outils Avancés */}
+          <Route path="/outils-avances/application-mobile" element={
+            <PrivateRoute>
+              <PrivateLayout>
+                <ApplicationMobileTerrain />
+              </PrivateLayout>
+            </PrivateRoute>
+          } />
 
-              <Route path="/outils-avances/messagerie" element={
-                <PrivateRoute>
-                  <PrivateLayout>
-                    <MessagerieEquipe />
-                  </PrivateLayout>
-                </PrivateRoute>
-              } />
+          <Route path="/outils-avances/messagerie" element={
+            <PrivateRoute>
+              <PrivateLayout>
+                <MessagerieEquipe />
+              </PrivateLayout>
+            </PrivateRoute>
+          } />
 
-              <Route path="/outils-avances/geolocalisation" element={
-                <PrivateRoute>
-                  <PrivateLayout>
-                    <Geolocalisation />
-                  </PrivateLayout>
-                </PrivateRoute>
-              } />
+          <Route path="/outils-avances/geolocalisation" element={
+            <PrivateRoute>
+              <PrivateLayout>
+                <Geolocalisation />
+              </PrivateLayout>
+            </PrivateRoute>
+          } />
 
-              {/* Settings */}
-              <Route path="/settings" element={
-                <PrivateRoute>
-                  <PrivateLayout>
-                    <Settings />
-                  </PrivateLayout>
-                </PrivateRoute>
-              } />
+          {/* Settings */}
+          <Route path="/settings" element={
+            <PrivateRoute>
+              <PrivateLayout>
+                <Settings />
+              </PrivateLayout>
+            </PrivateRoute>
+          } />
 
-              {/* Redirect */}
-              <Route path="/" element={<Navigate to="/dashboard" />} />
-            </Routes>
-          </ToastProvider>
-        </ProjectsProvider>
-      </AuthProvider>
+          {/* Redirect */}
+          <Route path="/" element={<Navigate to="/dashboard" />} />
+        </Routes>
+      </ToastProvider>
     </Router>
   )
 }
